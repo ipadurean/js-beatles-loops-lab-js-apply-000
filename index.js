1239c3ef5d628fb937b8513dbb9f52bc244d49cf
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-04-29 23:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+      var result = [];
+      for ( var i = 0; i<musicians.length; i++) {
+        for (var k = 0; k< instruments.length; k++) {
+          if(i===k) {
+              result.push(musicians[i] + " plays " + instruments[k]);
+          }
+        }
+      }
+      return result;
+    }
+  function johnLennonFacts (facts) {
+    var i = 0;
+    while (i < facts.length) {
+      facts[i] = facts[i]+"!!!";
+      i++
+    }
+    return facts;
+  }
+  
